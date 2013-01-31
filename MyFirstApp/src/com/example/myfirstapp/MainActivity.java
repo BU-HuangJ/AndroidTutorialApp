59be21145c01178d0ae5fa8b8b2cc0753ee8a10b
Joseph Huang <joseph_huang@baylor.edu>
2013-01-31 12:07:40 -0600

    Fixed up the main page Added an image button
    TODO:
    separate the display message activity from the main activity
    Add a full sized image

--- MyFirstApp/src/com/example/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/example/myfirstapp/MainActivity.java
@@ -6,11 +6,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-	StringBuilder stringBuilder = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +25,19 @@ public class MainActivity extends Activity {
     
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
-    	/*
         // Do something in response to button
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-        /*/
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	if (this.stringBuilder.length() > 0) {
-    		this.stringBuilder.append("\n");
-    	}
-    	this.stringBuilder.append( editText.getText().toString() );
-    	editText.setText("");
-    	TextView textView = (TextView) findViewById(R.id.text_box);
-	    textView.setText(this.stringBuilder.toString());
-        //*/
+    }
+    
+    /** Called when the user clicks the Send button */
+    public void chatNow(View view) {
+        // Do something in response to button
+    	Intent intent = new Intent(this, ChatActivity.class);
+        startActivity(intent);
     }
     
     @Override
